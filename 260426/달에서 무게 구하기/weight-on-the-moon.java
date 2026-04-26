6de0fa28007a443f589d18f3a7f4c55ec66e3de0
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-04-26 19:16:13 +0900

    Title: 달에서 무게 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260426/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int chu = 13;
+        double moon = 0.165;
+        double answer = chu * moon;
+        System.out.printf(chu+" * "+moon+"000"+" = "+answer+"000");
+    }
+}
