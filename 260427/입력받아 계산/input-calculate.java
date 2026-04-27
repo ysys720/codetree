fb097525fc94c9e3388932374a8340cf9dd7aad0
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-04-27 21:31:15 +0900

    Title: 입력받아 계산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/입력받아 계산/input-calculate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int a = input.nextInt();
+       int answer = a + 2;
+       System.out.print(answer);
+    }
+}
