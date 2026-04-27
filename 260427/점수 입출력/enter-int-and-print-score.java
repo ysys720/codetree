80ed41f59b2fb91301341e540cc9e2306b57d000
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-04-27 21:27:26 +0900

    Title: 점수 입출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        System.out.print("Your score is "+N+" point.");
+        
+    }
+}
