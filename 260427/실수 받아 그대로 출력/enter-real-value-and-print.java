15f866c5626bdc5e37eda2f7b275bcd178d42d26
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-04-27 21:44:53 +0900

    Title: 실수 받아 그대로 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       double N = input.nextDouble();
+       System.out.print(String.format("%.2f",N));
+    }
+}
