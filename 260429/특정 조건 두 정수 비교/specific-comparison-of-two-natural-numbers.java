a76604c9b29ef4f64b3733dd0fc061245d456bb2
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-04-29 22:17:26 +0900

    Title: 특정 조건 두 정수 비교, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
+++ 260429/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -1,13 +1,26 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        if ( B > A) {
-            System.out.print("1 0");
+
+        int less;
+        int equal;
+
+        if (A < B) {
+            less = 1;
         } else {
-            System.out.print("0 1");
+            less = 0;
         }
+
+        if (A == B) {
+            equal = 1;
+        } else {
+            equal = 0;
+        }
+
+        System.out.println(less + " " + equal);
     }
 }
