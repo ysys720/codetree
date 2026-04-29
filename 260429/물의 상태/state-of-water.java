80576a85c03096568089afe19986f1837a8cdb21
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-04-29 22:23:43 +0900

    Title: 물의 상태, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260429/물의 상태/state-of-water.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String tem;
+        if (n < 0) {
+            tem = "ice";
+        } else if (n>100) {
+            tem = "vapor";
+        } else {
+            tem = "water";
+        }
+
+        System.out.print(tem);
+    }
+}
