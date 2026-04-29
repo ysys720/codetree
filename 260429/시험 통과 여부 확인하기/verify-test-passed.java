bad3f59423e68cc92ca0448816d529a0cc9c93c9
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-04-29 22:04:17 +0900

    Title: 시험 통과 여부 확인하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260429/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N>=80) {
+            System.out.print("pass");
+        } else {
+            System.out.print(80-N+" ore score");
+        }
+    }
+}
