01701d5a46c9b4e656eda9f597f1b440790ab640
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-04-30 10:43:02 +0900

    Title: 문자 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260430/문자 출력하기/print-text.java
+++ 260430/문자 출력하기/print-text.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine();
         for (int i = 0; i<8; i++) {
-            System.out.print("G");
+            System.out.print(A);
         }
     }
 }
