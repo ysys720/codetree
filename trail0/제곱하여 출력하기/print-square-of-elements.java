120df96658dd442bfc796e92b6dae114f9aceac9
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-05-02 21:43:26 +0900

    [Passed] 제곱하여 출력하기 (133ms, 10MB)

---
+++ trail0/제곱하여 출력하기/print-square-of-elements.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] * arr[i]+" ");
+        }
+    }
+}
