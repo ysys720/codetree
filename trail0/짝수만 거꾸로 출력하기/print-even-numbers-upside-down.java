6754f622cc07018c75963e059900655cd2495411
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-05-03 21:33:33 +0900

    [Passed] 짝수만 거꾸로 출력하기 (143ms, 10MB)

---
+++ trail0/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
