7405b0b1e81f634a0dbdbd6ac90988cd3cd04e4a
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-05-03 21:21:32 +0900

    [Passed] 따옴표 출력 (87ms, 10MB)

---
+++ trail0/따옴표 출력/print-quote.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String text ="It's a really simple sentence";
+        System.out.print("He says \""+text+"\".");
+    }
+}
