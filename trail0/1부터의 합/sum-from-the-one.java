b12e3cf1e9f22bbc1fac06ad04d5a51d96141bbd
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-05-04 22:09:23 +0900

    [Passed] 1부터의 합 (136ms, 10MB)

---
+++ trail0/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+            if (sum >= n) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
