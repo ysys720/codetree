623b14a58b6ec0e7f67bd582bdfaeabbb2f53075
Lee YongSang <74398207+ysys720@users.noreply.github.com>
2026-05-04 22:07:56 +0900

    [Passed] 친근하지 않은 수 (128ms, 10MB)

---
+++ trail0/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
